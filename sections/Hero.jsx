2d349941e2046de796508a205ea7519b29e9de01
Ayden Springer <37941646+WraithWinterly@users.noreply.github.com>
2023-05-15 21:08:43 -0400

    Submit your solution button

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -21,13 +21,22 @@ const Hero = () => {
           className={`${styles.innerWidth} mx-auto flex flex-col`}
         >
           <div className="flex justify-center items-center flex-col relative z-10">
-            <motion.h1 variants={textVariant(1.1)} className={styles.heroSubHeading}>
+            <motion.h1
+              variants={textVariant(1.1)}
+              className={styles.heroSubHeading}
+            >
               The Syndicate presents
             </motion.h1>
-            <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+            <motion.h1
+              variants={textVariant(1.1)}
+              className={styles.heroHeading}
+            >
               HackGPT
             </motion.h1>
-            <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+            <motion.h1
+              variants={textVariant(1.1)}
+              className={styles.heroHeading}
+            >
               The Global
             </motion.h1>
             <motion.div
@@ -41,17 +50,18 @@ const Hero = () => {
               variants={textVariant(1.2)}
               className="absolute w-full bottom-0 z-10 flex flex-row justify-center items-center text-center"
             >
-              <h3 className={styles.heroDescription}>Catalyzing Fortune 2000 Enterprises' Digital Transformation</h3>
+              <h3 className={styles.heroDescription}>
+                Catalyzing Fortune 2000 Enterprises' Digital Transformation
+              </h3>
             </motion.div>
-
 
             <motion.div
               variants={textVariant(1.2)}
-              className="w-full bottom-0 z-10 flex flex-row justify-center items-center text-center mt-4 md:mt-5 lg:mt-6"
+              className="w-full bottom-0 z-10 flex flex-col justify-center items-center text-center mt-4 md:mt-5 lg:mt-6 gap-8"
             >
               <button
                 type="button"
-                className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
+                className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px] w-[260px]"
                 onClick={() => setModalShown(true)}
               >
                 <img
@@ -63,12 +73,34 @@ const Hero = () => {
                   Join the Hackathon
                 </span>
               </button>
+              <Link
+                type="button"
+                className="flex items-center w-[260px] h-fit py-4 px-6 bg-purple-700 rounded-[32px] gap-[12px]"
+                href="https://docs.google.com/forms/d/e/1FAIpQLSeBH4YgEnwskU7Ixq8L3kjziMnhf4Bj3sX6THFw6YH7CrFaqQ/viewform"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="white"
+                    d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h9v2H5v14h14v-9h2v9q0 .825-.588 1.413T19 21H5Zm2-4h2v-7H7v7Zm4 0h2V7h-2v10Zm4 0h2v-4h-2v4Zm2-8V7h-2V5h2V3h2v2h2v2h-2v2h-2Zm-5 3Z"
+                  />
+                </svg>
+                <span className="font-normal text-[16px] text-white">
+                  Submit Your Solution
+                </span>
+              </Link>
             </motion.div>
           </div>
 
           <motion.div
             variants={fadeIn('up', 'tween', 0.2, 1)}
-            className="relative w-full md:-mt-[9px] sm:-mt-[10px] mt-0"
+            className="relative w-full md:-mt-[80px] sm:-mt-[10px] mt-0"
           >
             <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
 
